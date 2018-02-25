d0a654c7ea342371c1647252af6e66a0b06db6af
Hengchu <hengchu@Hengchus-iMac.fios-router.home>
2018-02-25 17:01:00 -0500

    [paper] more work on section 2

--- paper/knapsack.hs
+++ paper/knapsack.hs
@@ -1,4 +1,5 @@
 import Data.Map.Lazy
+import Data.Function
 
 main = return ()
 
@@ -11,11 +12,14 @@ vs = [60, 100, 120]
 w :: Int
 w = 50
 
+solutionPre :: Map (Int, Int) Int -> Map (Int, Int) Int
+solutionPre soln = fromList [ ((j, k),
+                               if (j-1) < 0 || k - ws !! j < 0
+                               then if ws !! j <= k then vs !! j else 0
+                               else max (soln ! (j-1, k))
+                                        (soln ! (j-1, k - ws !! j) + vs !! j)
+                              )
+                            | j <- [0..2], k <- [0..50] ]
+
 solution :: Map (Int, Int) Int
-solution = fromList [ ((j, k),
-                       if (j-1) < 0 || k - ws !! j < 0
-                       then 0
-                       else max (solution ! (j-1, k))
-                                (solution ! (j-1, k - ws !! j) + vs !! j)
-                      )
-                    | j <- [0..2], k <- [0..50] ]
+solution = fix solutionPre
