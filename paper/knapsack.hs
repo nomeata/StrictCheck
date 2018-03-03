4be735c1a5c56d2293b18564b45cfe57153ce85b
hengchu <hengchu.zhang@gmail.com>
2018-03-03 14:43:20 -0500

    [paper] more work on section 2

--- paper/knapsack.hs
+++ paper/knapsack.hs
@@ -1,25 +1,25 @@
-import Data.Map.Lazy
+import Data.Map.Lazy hiding (foldr)
 import Data.Function
 
 main = return ()
 
-ws :: [Int]
-ws = [10, 20, 30]
+weights :: [Int]
+weights = [10, 20, 30]
 
-vs :: [Int]
-vs = [60, 100, 120]
+values :: [Int]
+values = [60, 100, 120]
 
-w :: Int
-w = 50
+limit :: Int
+limit = 50
 
-solutionPre :: Map (Int, Int) Int -> Map (Int, Int) Int
-solutionPre soln = fromList [ ((j, k),
-                               if (j-1) < 0 || k - ws !! j < 0
-                               then if ws !! j <= k then vs !! j else 0
-                               else max (soln ! (j-1, k))
-                                        (soln ! (j-1, k - ws !! j) + vs !! j)
-                              )
-                            | j <- [0..2], k <- [0..50] ]
+solutionStep :: Map (Int, Int) Int -> Map (Int, Int) Int
+solutionStep soln =
+  fromList [((j, k), knapsack j k) | j <- [0 .. length weights-1], k <- [0 .. limit]]
+  where
+    knapsack j k = if j - 1 < 0 || k - weights !! j < 0
+                   then if weights !! j <= k then values !! j else 0
+                   else max (soln ! (j-1, k))
+                            (soln ! (j-1, k - weights !! j) + values !! j)
 
 solution :: Map (Int, Int) Int
-solution = fix solutionPre
+solution = fix solutionStep
